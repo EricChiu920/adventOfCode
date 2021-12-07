89d83480d89da699d5a3daa6db45381a947ffdfd
Kanae Kanae <EricChiu920@gmail.com>
2021-12-07 11:47:19 -0500

    clean up code of 2021/06

--- 2021/06.ts
+++ 2021/06.ts
@@ -2,60 +2,37 @@
 import { parseLines } from '../helpers/helper';
 
 type FishTimer = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
-type FishCounter = {
-  0: number;
-  1: number;
-  2: number;
-  3: number;
-  4: number;
-  5: number;
-  6: number;
-  7: number;
-  8: number;
-};
-
-const INITIAL_FISH_COUNTER: FishCounter = {
-  0: 0,
-  1: 0,
-  2: 0,
-  3: 0,
-  4: 0,
-  5: 0,
-  6: 0,
-  7: 0,
-  8: 0,
-};
-
-const simulateDay = (lanternFishList: FishCounter) => {
-  const newFishList: FishCounter = { ...INITIAL_FISH_COUNTER };
-
-  const newFish = lanternFishList[0];
-  for (let i = 0; i <= 7; i += 1) {
-    newFishList[i as FishTimer] = lanternFishList[(i + 1) as FishTimer];
-  }
-
-  newFishList[6] += newFish;
-  newFishList[8] = newFish;
-
-  return newFishList;
-};
 
 const main = async (totalDays: number) => {
   const lanternFishData = await parseLines('06.txt');
   const lanternFishList = lanternFishData[0].split(',').map((timer) => Number(timer));
-  const calculatedFish: { [key: string]: number; } = {};
+  const calculatedFish = {
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+  };
 
-  for (let i = 1; i <= 5; i += 1) {
-    let fishList: FishCounter = { ...INITIAL_FISH_COUNTER };
-    fishList[i as FishTimer] = 1;
-
-    for (let day = 1; day <= totalDays; day += 1) {
-      fishList = simulateDay(fishList);
-    }
-    calculatedFish[i] = Object.values(fishList).reduce((acc, curr) => acc + curr, 0);
+  for (let i = 0; i < lanternFishList.length; i += 1) {
+    calculatedFish[lanternFishList[i] as FishTimer] += 1;
   }
 
-  console.log(lanternFishList.reduce((acc, timer) => acc + calculatedFish[timer], 0));
+  for (let day = 1; day <= totalDays; day += 1) {
+    const newFish = calculatedFish[0];
+    for (let i = 0; i < 8; i += 1) {
+      calculatedFish[i as FishTimer] = calculatedFish[(i + 1) as FishTimer];
+    }
+
+    calculatedFish[6] += newFish;
+    calculatedFish[8] = newFish;
+  }
+
+  console.log(Object.values(calculatedFish).reduce((acc, fish) => acc + fish, 0));
 };
 
 main(256);
